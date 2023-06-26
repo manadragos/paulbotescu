2f35e05cdc0af797afc63c312207cd24e6fbad59
Dragos Manarcescu <dragos.manarcescu@gmail.com>
2023-06-26 14:57:21 +0300

    TEMP Fronts

--- api/server.js
+++ api/server.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const { json } = require("body-parser");
 const axios = require("axios");
+//var cloudinary = require('cloudinary');
+var cloudinary = require('cloudinary').v2;
 
 const app = express();
 
@@ -11,15 +13,47 @@ app.use(json());
 
 const { parsed: config } = dotenv.config();
 const BASE_URL = `https://api.cloudinary.com/v1_1/${config.CLOUD_NAME}`;
+const CLOUDINARY_URL=`cloudinary://${config.API_KEY}:${config.API_SECRET}@${config.CLOUD_NAME}`
+
+https://cloudinary.com/documentation/node_integration
+
+cloudinary.config({ 
+  cloud_name: `${config.CLOUD_NAME}`, 
+  api_key: `${config.API_KEY}`, 
+  api_secret: `${config.API_SECRET}`, 
+  secure: true
+});
+
+
 const auth = {
   username: config.API_KEY,
   password: config.API_SECRET,
 };
 
+/////////////////////////////////////
+// Gets details of an uploaded image
+/////////////////////////////////////
+const getAssetInfo = async (publicId) => {
+
+  // Return colors in the response
+  const options = {
+    colors: true,
+  };
+
+  try {
+      // Get details about the asset
+      const result = await cloudinary.api.resource(publicId, options);
+      console.log(result);
+      return result.colors;
+      } catch (error) {
+      console.error(error);
+  }
+};
+
 app.get("/photos", async (req, res) => {
   // const response = await axios.get(BASE_URL + "/resources/image", {
   const response = await axios.get(
-    BASE_URL + "/resources/search?expression=folder:Test",
+    BASE_URL + "/resources/search?expression=folder:Fronts",
     {
       auth,
       params: {
--- client-photo-gallery/src/components/ImageFolder.js
+++ client-photo-gallery/src/components/ImageFolder.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import "../App.css";
 
 const ImageFolder = ({ image }) => {
-  return <img src={image.url} alt={image.public_id}></img>;
+  return <img src={image.url} alt={image.public_id} style={{width: '200px'}}></img>;
 };
 
 export default ImageFolder;
